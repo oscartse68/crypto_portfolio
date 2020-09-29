dd93e7e57fc121a2c6d6ffd4960f2d12f75c5fb0
oscartmc <oscartse68@gmail.com>
2020-09-29 19:30:20 +0800

    add cel_historical_stats into cel class

--- celsius-network-balance-check.py
+++ celsius-network-balance-check.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import os
+import json
 from datetime import datetime
 
 pd.set_option('display.width', 500)
@@ -8,16 +9,21 @@ pd.set_option('display.max_columns', 15)
 
 
 class CelsiusNetworkAPI:
-    _base_url = "https://wallet-api.celsius.network"
+    __base_url = "https://wallet-api.celsius.network"
 
     def __init__(self, partner_token, user_token):
         self.partner_token = partner_token
         self.user_token = user_token
-        self.actions = {
+        self.coin_list = []
+        self.headers = {
+            "X-Cel-Api-Key": self.user_token,
+            "X-Cel-Partner-Token": self.partner_token
+        }
+        self.__actions = {
             "balance_summary": {
                 "description": "Check Wallet Balance Summary (USD)",
                 "param": "No parameters needed",
-                "url": "/wallet/interest"
+                "url": "/wallet/balance"
             },
             "balance_summary_coin": {
                 "description": "Check Wallet Balance Summary (in Kind)",
@@ -45,22 +51,25 @@ class CelsiusNetworkAPI:
             },
         }
 
-    def get_credentials(self):
-        return {
-            "X-Cel-Api-Key": self.user_token,
-            "X-Cel-Partner-Token": self.partner_token
-        }
-
     def show_wallet_action(self):
         print("Action -> Command")
-        for key, val in self.actions.items():
+        for key, val in self.__actions.items():
             print(val['description'], " -> ", key)
 
-    def celsius_request(self, command):
-        if command not in self.actions.keys():
+    def request(self, command: str, **kwargs):
+        if command not in self.__actions.keys():
             return "Error please input valid command"
         else:
-            requests.get(self._base_url+self.actions[command]['url'], headers=self.get_credentials())
+            return requests.get(self.__base_url+self.__actions[command]['url'], headers=self.headers)
+
+    def get_cel_historical_interest(self, coin_list: list, start_date: str, end_date: str) -> pd.DataFrame:
+        __celsian_stata_url = "https://raw.githubusercontent.com/Celsians/Google-Sheets/master/data.json"
+        response = requests.request("GET", __celsian_stata_url)
+        df = pd.DataFrame(response.json())
+        df = df[["Date"] + coin_list][1:]
+        df['date_obj'] = pd.to_datetime(df['Date'], format='%d.%m.%Y')
+        df = df.query('date_obj > "2020-07-01"').drop(columns=['Date']).reset_index(drop=True)
+        return df
 
 
 class CoinGecko:
@@ -81,18 +90,10 @@ class CoinGecko:
         return df
 
 
-def get_cel_historical_interest(token_balance: dict) -> pd.DataFrame:
-    url = "https://raw.githubusercontent.com/Celsians/Google-Sheets/master/data.json"
-    response = requests.request("GET", url)
-    df = pd.DataFrame(response.json())
-    df = df[["Date"] + list(token_balance.keys())][1:]
-    df['date_obj'] = pd.to_datetime(df['Date'], format='%d.%m.%Y')
-    df = df.query('date_obj > "2020-07-01"').drop(columns=['Date']).reset_index(drop=True)
-    return df
-
-
 def main():
-    cel_credentials = [os.path.join(os.getcwd(), x) for x in os.listdir(os.getcwd()) if "credentials" in x][0]
+    cel_credentials_path = [os.path.join(os.getcwd(), x) for x in os.listdir(os.getcwd()) if "credentials" in x][0]
+    cel_credentials = json.load(open(cel_credentials_path))
+    cel = CelsiusNetworkAPI(partner_token=cel_credentials['partner_token'], user_token=cel_credentials['user_token'])
 
 
 if __name__ == '__main__':
