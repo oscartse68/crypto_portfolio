f449fbebd187ff78a5f3679da5c0ff3df19b4268
oscartmc <oscartse68@gmail.com>
2020-09-30 11:35:03 +0800

    tried df_balance_summary

--- celsius-network-balance-check.py
+++ celsius-network-balance-check.py
@@ -10,6 +10,7 @@ pd.set_option('display.max_columns', 15)
 
 class CelsiusNetworkAPI:
     __base_url = "https://wallet-api.celsius.network"
+    __celsian_stat_url = "https://raw.githubusercontent.com/Celsians/Google-Sheets/master/data.json"
 
     def __init__(self, partner_token, user_token):
         self.partner_token = partner_token
@@ -60,15 +61,15 @@ class CelsiusNetworkAPI:
         if command not in self.__actions.keys():
             return "Error please input valid command"
         else:
-            return requests.get(self.__base_url+self.__actions[command]['url'], headers=self.headers)
+            return requests.get(self.__base_url+self.__actions[command]['url'], headers=self.headers).json()
 
-    def get_cel_historical_interest(self, coin_list: list, start_date: str, end_date: str) -> pd.DataFrame:
-        __celsian_stata_url = "https://raw.githubusercontent.com/Celsians/Google-Sheets/master/data.json"
-        response = requests.request("GET", __celsian_stata_url)
+    def get_cel_historical_interest(self, coin_list: list, start_date: str) -> pd.DataFrame:
+        response = requests.request("GET", self.__celsian_stat_url)
         df = pd.DataFrame(response.json())
         df = df[["Date"] + coin_list][1:]
         df['date_obj'] = pd.to_datetime(df['Date'], format='%d.%m.%Y')
-        df = df.query('date_obj > "2020-07-01"').drop(columns=['Date']).reset_index(drop=True)
+        df = df.query(f'date_obj > "{start_date}"').drop(columns=['Date']).reset_index(drop=True)
+        df = df[['date_obj']+[x for x in df.columns if x != "date_obj"]]
         return df
 
 
@@ -94,6 +95,10 @@ def main():
     cel_credentials_path = [os.path.join(os.getcwd(), x) for x in os.listdir(os.getcwd()) if "credentials" in x][0]
     cel_credentials = json.load(open(cel_credentials_path))
     cel = CelsiusNetworkAPI(partner_token=cel_credentials['partner_token'], user_token=cel_credentials['user_token'])
+    df_balance_summary = pd.DataFrame(
+        cel.request("balance_summary").json()['balance'].items(),
+        columns=['coin', 'balance_in_kind']
+    ).astype({"balance_in_kind": "float"}).query("balance_in_kind != 0")
 
 
 if __name__ == '__main__':
